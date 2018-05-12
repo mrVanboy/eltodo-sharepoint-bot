4d74bcd1cfea18e3fc5d9f19eb7552b224f8c202
Ivan Boyarkin <boyarkini@eltodo.cz>
2018-05-12 20:52:58 +0200

    Add FullHD screenshot saving to artifacts, set log to errors only

--- src/sharepoint-bot/chrome/main.go
+++ src/sharepoint-bot/chrome/main.go
@@ -7,6 +7,11 @@ import (
 	"context"
 	"time"
 	"sharepoint-bot/cfg"
+	"io/ioutil"
+	"os"
+	"fmt"
+	"github.com/chromedp/cdproto/emulation"
+	"github.com/chromedp/cdproto/cdp"
 )
 
 func LoadAnnouncements() (string, error) {
@@ -16,7 +21,9 @@ func LoadAnnouncements() (string, error) {
 	defer cancel()
 
 	// create chrome
-	c, err := chromedp.New(ctxt, chromedp.WithTargets(client.New(client.URL(cfg.Get().ChromeUrl)).WatchPageTargets(ctxt)), chromedp.WithLog(log.Printf))
+	c, err := chromedp.New(ctxt, chromedp.WithTargets(client.New(client.URL(
+		cfg.Get().ChromeUrl)).WatchPageTargets(ctxt)),
+		chromedp.WithErrorf(log.Printf))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,14 +36,40 @@ func LoadAnnouncements() (string, error) {
 	if err != nil {
 		return ``, err
 	}
+	saveScreenshot(screenshotBuf)
 	return res, nil
+}
+
+func saveScreenshot(screenshotBuf []byte) {
+	err := os.MkdirAll(`artifacts`, os.ModePerm)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can't create artifacts folder: %s\n", err)
+		return
+	}
+	err = ioutil.WriteFile(`artifacts/output.png`, screenshotBuf, os.ModePerm)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "can't create screenshot: %s\n", err)
+		return
+	}
 }
 
 func getAnnouncements(site string, res *string, screenshotBuf *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(site),
-		chromedp.WaitReady(`.announcements-list`),
+		chromedp.WaitVisible(`.announcements-list`),
 		chromedp.OuterHTML(`.announcements-list`, res, chromedp.NodeReady),
+		setViewportAndScale(1920, 1080),
+		chromedp.Sleep(5 * time.Second),
 		chromedp.CaptureScreenshot(screenshotBuf),
+	}
+}
+
+func setViewportAndScale(w, h int64) chromedp.ActionFunc {
+	return func(ctxt context.Context, ha cdp.Executor) error {
+		err := emulation.SetDeviceMetricsOverride(w, h, 1, false).Do(ctxt, ha)
+		if err != nil {
+			return err
+		}
+		return nil
 	}
 }
